32e7c554aea2626b8723bf6215425cb0c08ba7be
singhpragnesh89 <71432507+singhpragnesh89@users.noreply.github.com>
2022-05-13 20:11:41 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/data/data/com.termux/files/usr/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is Coded by Pragnesh Singh"
+echo "---------------------------------------------------------------------------------"
+echo
+echo
+echo
+echo "."
+echo
+apt update -y
+apt install python -y
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install wget -y
+apt install wcalc -y 
+apt install openssl -y
+apt install openssh -y
+apt install bmon -y
+echo
+echo
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+echo
+exit
